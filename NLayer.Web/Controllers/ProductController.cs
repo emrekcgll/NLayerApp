b94c732c423ef3840961a239a403c3062ad42ee7
Emre Koçoğlu <emrekcgll@gmail.com>
2023-09-06 22:53:44 +0300

    -

--- NLayer.Web/Controllers/ProductController.cs
+++ NLayer.Web/Controllers/ProductController.cs
@@ -1,7 +1,9 @@
 ﻿using AutoMapper;
+using Humanizer;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using NLayer.Core.DTOs;
+using NLayer.Core.Entities;
 using NLayer.Repository;
 
 namespace NLayer.Web.Controllers
@@ -22,6 +24,55 @@ namespace NLayer.Web.Controllers
             var productsWithCategory = await _context.Products.Include(x => x.Category).ToListAsync();
             var productsWithCategoryDTO = _mapper.Map<List<ProductWithCategoryDTO>>(productsWithCategory);
             return View(productsWithCategoryDTO);
+        }
+
+        public async Task<IActionResult> Details(int id)
+        {
+            var product = await _context.Products.FindAsync(id);
+            var productDTO = _mapper.Map<ProductDTO>(product);
+            return View(productDTO);
+        }
+
+        public async Task<IActionResult> Delete(int id)
+        {
+            var product = _context.Products.Find(id);
+            if (product != null)
+            {
+                _context.Remove(product);
+            }
+            return RedirectToAction(nameof(Index));
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Add()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Add(ProductDTO dto)
+        {
+            await _context.AddAsync(_mapper.Map<Product>(dto));
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Update(int id)
+        {
+            var existingProduct = await _context.Products.FindAsync(id);
+            var existingProductDTO= _mapper.Map<ProductDTO>(existingProduct);
+            return View(existingProductDTO);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Update(ProductDTO dto)
+        {
+            var existingProduct = await _context.Products.FindAsync(dto.Id);
+            _mapper.Map<ProductDTO>(existingProduct); //_mapper.Map(dto, existingProduct);
+            _context.Update(existingProduct);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
         }
     }
 }
